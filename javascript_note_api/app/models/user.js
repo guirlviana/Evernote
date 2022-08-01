430274a4b6a2bffea51e5969c2bea1a13ab31e6d
guirlviana <girlviana@gmail.com>
2022-08-01 10:58:36 -0300

    fix missing ')'

--- javascript_note_api/app/models/user.js
+++ javascript_note_api/app/models/user.js
@@ -6,6 +6,6 @@ let userSchema = new mongoose.Schema({
     email: {type: String, required: true},
     created_at: { type: Date, default: Date.now },
     updated_at: {type: Date, default: Date.now },
-}
+})
 
 module.exports = mongoose.model('User', userSchema);
