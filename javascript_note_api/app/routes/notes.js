9c68f5a9ab546eacd46eab2b1751c1c6374b0e4e
guirlviana <girlviana@gmail.com>
2022-08-01 16:22:37 -0300

    feat route to add notes

---
+++ javascript_note_api/app/routes/notes.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var router = express.Router();
+const Note = require("../models/notes");
+
+router.post("/", async (req, res) => {
+  const { title, body } = req.body;
+  try {
+    let note = new Note({ title: title, body: body, author: req.user._id });
+    await note.save();
+    res.status(200).json(note);
+  } catch (error) {
+    res.status(500).json({ error: "Not possible to create new note" });
+  }
+});
+
+module.exports = router;
